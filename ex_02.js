80cb20876afcf69508e855d85c7db374194620e9
Fabio Henrique <fabiotrevizolli8@gmail.com>
2025-05-29 22:49:05 -0300

    inverte o Array

---
+++ ex_02.js
@@ -0,0 +1,9 @@
+function inverterArray(arr) {
+    let inverso = [];
+    for (let i = arr.length - 1; i >= 0; i--) {
+        inverso.push(arr[i]);
+    }
+    console.log(inverso);
+}
+
+inverterArray([1, 2, 3, 4, 5, 6]); 

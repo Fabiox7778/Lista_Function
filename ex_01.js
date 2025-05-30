736666fb00c5d6ef2194dacda56666576b495c30
Fabio Henrique <fabiotrevizolli8@gmail.com>
2025-05-29 22:50:18 -0300

    Verifica se é primo

--- ex_01.js
+++ ex_01.js
@@ -1,13 +1,13 @@
 function ehPrimo(numero) {
     if (numero <= 1) {
-        console.log("false")
+        console.log("false");
         return;
     } for (let i = 2; i < numero; i++) {
         if (numero % i === 0) {
-            console.log("false")
-            return
+            console.log("false");
+            return;
         }
-    } console.log("true")
+    } console.log("true");
 }
 
 ehPrimo(853)

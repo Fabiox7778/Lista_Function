bc212ed4d12d60eb0bad4885796ad523d2c1c7ed
Fabiox7778 <fabiorabello08@gmail.com>
2025-05-30 13:50:58 -0300

    Conta palavras em uma frase

--- ex_04.js
+++ ex_04.js
@@ -1,1 +1,6 @@
-//Ainda não fiz 
+function contaPalavras(frase) {
+    let arr = frase.toString().split(' ')
+    let words = arr.length;
+    console.log(words)
+}
+contaPalavras("O lisboa é fiotao do fabioo")

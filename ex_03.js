012ac526dba4bbc45eced44c25e0a967acdc1528
Fabio Henrique <fabiotrevizolli8@gmail.com>
2025-05-29 23:51:51 -0300

    Mostra o segundo maior valor

--- ex_03.js
+++ ex_03.js
@@ -1,3 +1,20 @@
 function segundoMaior(arr) {
-    
+    if (arr.length < 2) {
+        return arr[0]
+    }
+
+    let maior = arr[0];
+    let segundo = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > maior) {
+            segundo = maior;
+            maior = arr[i];
+        } else if (arr[i] < maior && (segundo === maior || arr[i] > segundo)) {
+            segundo = arr[i];
+        }
+    }
+    console.log(segundo === maior ? maior : segundo);
 }
+
+segundoMaior([100, 50, 80]);
